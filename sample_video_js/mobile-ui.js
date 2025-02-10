fcd42f3de2f0fd8c28a4eaa27b0c727fe042d5ba
emintolgahanpolat <emintolgahanpolat@gmail.com>
2025-02-10 09:22:06 +0300

    -

---
+++ sample_video_js/mobile-ui.js
@@ -0,0 +1,101 @@
+// Player Mobile UI Initialization
+function addMobileUI(player) {
+    const video = player.el();
+
+
+    const mobileControlDiv = document.createElement('div');
+    mobileControlDiv.classList.add('mobile-vjs-controls');
+
+    // Rewind button
+    const rewindBtn = document.createElement('button');
+    rewindBtn.className = 'vjs-replay-button vjs-control vjs-button';
+    const rewindSpan = document.createElement('span')
+    rewindSpan.classList.add('vjs-icon-placeholder')
+    rewindBtn.appendChild(rewindSpan)
+    const rewindContent = document.createElement('span')
+    rewindContent.classList.add('vjs-control-text')
+    rewindContent.innerText = "Rewind"
+    rewindBtn.appendChild(rewindContent)
+
+    rewindBtn.addEventListener('click', () => {
+        player.currentTime(player.currentTime() - 10);
+    });
+
+
+    // Forward button
+    const forwardBtn = document.createElement('button');
+    forwardBtn.className = 'vjs-forward-button vjs-control vjs-button';
+    const forwardSpan = document.createElement('span')
+    forwardSpan.classList.add('vjs-icon-placeholder')
+    forwardBtn.appendChild(forwardSpan)
+    const forwardContent = document.createElement('span')
+    forwardContent.classList.add('vjs-control-text')
+    forwardContent.innerText = "Forward"
+    forwardBtn.appendChild(forwardContent)
+
+    forwardBtn.addEventListener('click', () => {
+        player.currentTime(player.currentTime() + 10);
+    });
+
+    // Append buttons to controls div
+    mobileControlDiv.appendChild(rewindBtn);
+    mobileControlDiv.appendChild(player.controlBar.playToggle.el());
+    mobileControlDiv.appendChild(forwardBtn);
+
+    // Append controls to the video container
+    video.appendChild(mobileControlDiv);
+
+
+    // Top Controls Container
+    const topControlDiv = document.createElement('div');
+    topControlDiv.classList.add('mobile-vjs-controls-top');
+
+    // Kontrollü ekleme (Butonlar yoksa hata almamak için)
+
+    if (player.controlBar.subsCapsButton) {
+        topControlDiv.appendChild(player.controlBar.subsCapsButton.el());
+    }
+    if (player.controlBar.audioTrackButton) {
+        topControlDiv.appendChild(player.controlBar.audioTrackButton.el());
+    }
+    if (player.controlBar.pictureInPictureToggle) {
+        topControlDiv.appendChild(player.controlBar.pictureInPictureToggle.el());
+    }
+    if (player.controlBar.fullscreenToggle) {
+        topControlDiv.appendChild(player.controlBar.fullscreenToggle.el());
+    }
+
+
+
+
+    video.appendChild(topControlDiv);
+
+
+    // Top Controls Container
+    const leftControlDiv = document.createElement('div');
+    leftControlDiv.classList.add('mobile-vjs-controls-left');
+
+    // Kontrollü ekleme (Butonlar yoksa hata almamak için)
+    var volumePanel = player.controlBar.volumePanel.el()
+    volumePanel.classList.add("mobile-vjs-volume-panel-vertical")
+    leftControlDiv.appendChild(volumePanel);
+
+
+
+
+    video.appendChild(leftControlDiv);
+
+    // player.on('useractive', () => {
+    //     mobileControlDiv.style.display = 'flex';  // Show controls when user is active
+    // });
+
+    // // Listen for when user stops interacting with the player
+    // player.on('userinactive', () => {
+    //     mobileControlDiv.style.display = 'none';  // Hide controls when user is inactive
+    // });
+
+
+
+
+}
+
